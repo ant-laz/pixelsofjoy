fdb995e302cfef2ab15547bc240a28408817297f
Esraa Elewa <esraaelewa@google.com>
2026-04-16 11:46:21 +0100

    Timer formatting

--- v-4-vengeance/scripts/game_engine.gd
+++ v-4-vengeance/scripts/game_engine.gd
@@ -9,8 +9,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var time_remaining = snapped($Timer.time_left, 0.1)
-	TimerLabel.text = str(time_remaining)
+	var time_remaining = $Timer.time_left
+	var minutes = int(time_remaining / 60)
+	var seconds = int(fmod(time_remaining, 60))
+	var msec = int(fmod(time_remaining, 1) * 10)
+	
+	# 3. Format the string (MM:SS:CC)
+	# %02d ensures every section always has two digits
+	TimerLabel.text = "%02d:%02d:%01d" % [minutes, seconds, msec]
+	#TimerLabel.text = time_string
 
 
 func _on_timer_timeout() -> void:
